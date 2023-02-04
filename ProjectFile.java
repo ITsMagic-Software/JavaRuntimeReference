6160ab3c48b7a7be727224be4d31f002e47096ab
Lucas Leandro <lucasleandro1805@gmail.com>
2023-02-03 21:39:34 -0300

    Updates

---
+++ ProjectFile.java
@@ -0,0 +1,41 @@
+package JAVARuntime;
+
+//
+
+
+/**
+ * @Author Lucas Leandro (ITsMagic Founder)
+ */
+@ClassCategory(cat ={"Files"})
+@UnimplementedDoc
+public class ProjectFile
+//
+{
+    //
+
+    @UnimplementedDoc
+    public ProjectFile(String format) {
+        //
+    }
+
+    @UnimplementedDoc
+    public String getFormat() {
+        //
+        return null;
+        //
+
+        //
+    }
+    @UnimplementedDoc
+    public InputStream getInputStream(){
+        //
+        return null;
+        //
+
+        //
+    }
+    @UnimplementedDoc
+    public String toString() {
+        return getClass().getName();
+    }
+}
---
+++ UnimplementedDoc.java
@@ -0,0 +1,11 @@
+package JAVARuntime;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Don't add this class to the documentation
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UnimplementedDoc {
+}
