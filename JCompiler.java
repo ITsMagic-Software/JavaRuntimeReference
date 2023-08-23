e05622612aa51556165eb24bf371a273af6f1c56
Lucas Leandro <lucasleandro1805@gmail.com>
2023-08-23 12:05:36 -0300

    Updates

---
+++ JCompiler.java
@@ -0,0 +1,98 @@
+package JAVARuntime;
+
+//
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * @Author Lucas Leandro (ITsMagic Founder)
+ */
+@ClassCategory(cat ={"Java"})
+public class JCompiler {
+
+    public static List<JClass> getAllClasses(){
+        //
+        return null;
+        //
+
+        //
+    }
+
+    public static List<JClass> getEngineClasses(){
+        //
+        return null;
+        //
+
+        //
+    }
+    public static int engineClassCount(){
+        //
+        return 0;
+        //
+
+        //
+    }
+    public static JClass engineClassAt(int idx){
+        //
+        return null;
+        //
+
+        //
+    }
+
+    public static List<JClass> getUsersClasses(){
+        //
+        return null;
+        //
+
+        //
+    }
+    public static int userClassCount(){
+        //
+        return 0;
+        //
+
+        //
+    }
+    public static JClass userClassAt(int idx){
+        //
+        return null;
+        //
+
+        //
+    }
+
+    public static JClass findOfficialClass(String fullName){
+        //
+        return null;
+        //
+
+        //
+    }
+    public static JClass findUserClass(String fullName){
+        //
+        return null;
+        //
+
+        //
+    }
+
+    public static boolean isCompiling(){
+        //
+        return false;
+        //
+
+        //
+    }
+
+    public static void addListener(Object object){
+        //
+    }
+    public static void removeListener(Object object){
+        //
+    }
+}
+
+
